7c09b21677db95243b68b7a6c1ec4a0e636d9e31
Francisco Camargo <francisco.camargo@nxp.com>
2023-12-22 17:48:30 -0600

    Add pydantic models

---
+++ sqlalchemy/schemas.py
@@ -0,0 +1,33 @@
+'''
+Use Pydantic models/schemas to validate the data coming in
+and out of the FastAPI application.
+'''
+
+from datetime import datetime
+
+from pydantic import (
+    BaseModel,
+    Field,
+)
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    publication_date: datetime = Field(default_factory=datetime.now)
+
+    class Config:
+        orm_mode = True
+
+
+class PostPartialUpdate(BaseModel):
+    title: str | None = None
+    content: str | None = None
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostRead(PostBase):
+    id: int
